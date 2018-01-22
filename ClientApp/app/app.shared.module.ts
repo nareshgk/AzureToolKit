9c4c4b09b3ffbae3ecd0e8219caea91de6f47c93
Naresh Gaikwad <nareshgk@outlook.com>
2018-01-22 17:18:01 +0530

    Added serach

--- ClientApp/app/app.shared.module.ts
+++ ClientApp/app/app.shared.module.ts
@@ -8,15 +8,16 @@ import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
+import { SearchComponent } from './components/search/search.component';
+
 
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
-        CounterComponent,
         FetchDataComponent,
-        HomeComponent
+        HomeComponent,
+        SearchComponent
     ],
     imports: [
         CommonModule,
@@ -25,8 +26,8 @@ import { CounterComponent } from './components/counter/counter.component';
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'search', component: SearchComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ]
